c0e43e9248ea450532cfe9fa116fce5effa63880
Anuj Kumar Jha <anujkumarjha4303@gmail.com>
2025-11-08 16:22:24 +0530

    Fix: npm module missing fix

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
   onClick: () => void
 }
 export const Button = (props: ButtonProps) => {
-
+ 
   return (
     <button onClick={props.onClick} data-testid="button-test">Button</button>
   )
