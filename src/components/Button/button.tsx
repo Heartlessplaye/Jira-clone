d1fc7c27d9d0eeb91633b83a05007b05aeb475a0
Anuj Kumar Jha <anujkumarjha4303@gmail.com>
2025-11-08 14:17:40 +0530

    fixing test

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -5,13 +5,6 @@ interface ButtonProps {
 }
 export const Button = (props: ButtonProps) => {
 
-
-  const handler = () => {
-    console.log("Button click handler");
-  }
-  if (!props.onClick) {
-    return <button onClick={props.onClick} data-testid="button-test"> button </button>
-  }
   return (
     <button onClick={props.onClick} data-testid="button-test">Button</button>
   )
