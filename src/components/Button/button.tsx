4307f667137c0c9fd68bf45c69f4791b8d7a2c40
Anuj Kumar Jha <anujkumarjha4303@gmail.com>
2025-11-08 13:59:54 +0530

    evil commit

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -4,8 +4,16 @@ interface ButtonProps {
   onClick: () => void
 }
 export const Button = (props: ButtonProps) => {
+
+
+  const handler = () => {
+    console.log("Button click handler");
+  }
+  if (!props.onClick) {
+    return <button onClick={props.onClick} data-testid="button-test"> button </button>
+  }
   return (
-    <button onClick={props.onClick} data-testid = "button-test">Button</button>
+    <button onClick={props.onClick} data-testid="button-test">Button</button>
   )
 }
 
